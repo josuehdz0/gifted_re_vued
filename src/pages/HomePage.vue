d11d0eb5523f63b1df8a8b12a07b91adde2e513e
Josue Hernandez <josuehdz0@gmail.com>
2023-02-22 09:19:20 -0700

    started opening

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,32 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    
+ <div class="container-fluid">
+  <div class="row">
+    <div class="col-4">search</div>
+    <div class="col-8">
+      <div class="row pt-2">
+        <h2 class="text-center">GIFTS</h2>
+      </div>
+      <div class="row">
+        <div class="col-4" v-for="gift in gifts">
+          <div class="card giftcard my-2">
+            <img :src="gift.url" alt="img" class="img-fluid crdimg rounded-top">
+            <div class="card-body crdbdy">
+              <h6>{{gift.tag}}</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+ </div>
 </template>
 
 <script>
 import { logger } from "../utils/Logger.js";
 import Pop from "../utils/Pop.js";
 import {giftsServices} from "../services/GiftsServices.js"
-import { onMounted } from "vue";
+import { onMounted, computed } from "vue";
+import { AppState } from "../AppState.js";
 
 export default {
   setup() {
@@ -25,12 +43,37 @@ export default {
       logger.log('hi from homepage')
       getGifts()
     })
-    return {}
+    return {
+      gifts: computed(()=> AppState.gifts),
+
+      async openGift(gift){
+        try {
+          await giftsServices.openGift(gift)
+        } catch (error) {
+          Pop.error(error.message)
+          logger.error(error)
+        }
+      }
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
+
+.giftcard{
+  max-height: 45vh;
+}
+
+.crdimg{
+ height: 20vh;
+ object-fit: cover;
+}
+
+.crdbdy{
+ height: 8vh;
+ object-fit: cover;
+}
 .home {
   display: grid;
   height: 80vh;
@@ -38,16 +81,5 @@ export default {
   text-align: center;
   user-select: none;
 
-  .home-card {
-    width: 50vw;
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
 }
 </style>
