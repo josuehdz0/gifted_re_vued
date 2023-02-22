a9c6abd4ba8a040d023fc1ed1b67872aca7ce28e
Josue Hernandez <josuehdz0@gmail.com>
2023-02-22 09:37:25 -0700

    opening gifts?

--- src/services/GiftsServices.js
+++ src/services/GiftsServices.js
@@ -1,3 +1,5 @@
+import { AppState } from "../AppState.js"
+import { Gift } from "../models/Gift.js"
 import { logger } from "../utils/Logger.js"
 import { giftedApi } from "./AxiosService.js"
 
@@ -6,6 +8,14 @@ class GiftsServices{
   async getGifts(){
     const res = await giftedApi.get('gifts')
     logger.log('getting gifts', res.data)
+    AppState.gifts = res.data.map(g => new Gift(g))
+  }
+
+  async openGift(gift){
+    const res = await giftedApi.put(`gifts/${gift.id}`,{opened:true} )
+    logger.log('opening gift', res.data)
+    AppState.gifts = 
+    
   }
 }
 export const giftsServices = new GiftsServices()
